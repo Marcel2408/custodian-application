801c8a1cbae087d3456e6a798160c9bc6458f748
Marcel Subirana <msubiranac@gmail.com>
2021-04-14 19:22:43 +0200

    test(app): add App unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme'
+import App from './App'
+import Header from './components/organisms/header/header'
+import PageTemplate from './components/templates/page/page'
+/**
+ * Setup fn for the App component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />)
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders page template without error', () => {
+    const wrapper = setup()
+    const app = wrapper.find(PageTemplate)
+    expect(app).toHaveLength(1)
+})
+
+test('renders header without error', () => {
+    const wrapper = setup()
+    const header = wrapper.find(Header)
+    expect(header).toHaveLength(1)
+})
--- src/components/organisms/task/task.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import Task from './task'
-
-const setup = (task = {}) => {
-    const wrapper = mount(<Task />)
-}
-
-test('jimy', () => {})
